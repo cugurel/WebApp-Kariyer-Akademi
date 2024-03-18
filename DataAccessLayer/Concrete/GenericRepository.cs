6dc88110267159dfa512224f06d41d865d3cd4c4
Cugurel <cugurel7@gmail.com>
2024-03-18 20:59:17 +0300

    generic repository

---
+++ DataAccessLayer/Concrete/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class, new()
+    {
+        public void Delete(T t)
+        {
+            using var c = new Context();
+            c.Remove(t);
+        }
+
+        public List<T> GetAll()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            using var c = new Context();
+            return c.Set<T>().Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            using var c = new Context();
+            c.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            using var c = new Context();
+            c.Update(t);
+        }
+    }
+}
