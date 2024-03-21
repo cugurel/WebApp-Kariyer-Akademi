55b35623ce90edc1c83ff9546fb124086b47a7a6
Cugurel <cugurel7@gmail.com>
2024-03-21 19:40:31 +0300

    category manager

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        void CatagoryAdd(Category category);
+
+    }
+}
---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using DataAccessLayer.Concrete;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        CategoryRespository categoryRespository = new CategoryRepository();
+        public void CatagoryAdd(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CatagoryUpdate(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
