a4e4a6973a1e26ea349dd9e1b37567bb9676a249
Cugurel <cugurel7@gmail.com>
2024-03-27 21:08:50 +0300

    api den getirme

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Entity.Concrete;
 using Entity.Concrete.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using WebApp.Models;
 
@@ -10,23 +11,39 @@ namespace WebApp.Controllers
     public class HomeController : Controller
     {
         Context context = new Context();
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var result = (from mv in context.Movies
-                            join ct in context.Categories on mv.CategoryId equals ct.Id
-                            select new MovieCategoryDto
-                            {
-                                Id = mv.Id,
-                                CategoryId = mv.CategoryId,
-                                Director = mv.Director,
-                                ImageUrl = mv.ImageUrl,
-                                ImdbRate = mv.ImdbRate,
-                                Name = mv.Name,
-                                CategoryName = ct.Name
-                            });
-                
-            return View(result);
-            
+            List<MovieCategoryDto> movies = new List<MovieCategoryDto>();
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage message = await client.
+                    GetAsync("https://localhost:7101/api/Movie/AllMovies"))
+                {
+                    using (HttpContent content = message.Content)
+                    {
+                        var data = await content.ReadAsStringAsync();
+                        movies = JsonConvert.DeserializeObject<List<MovieCategoryDto>>(data);
+                    }
+                }
+            }
+
+            return View(movies);
+
+            //var result = (from mv in context.Movies
+            //              join ct in context.Categories on mv.CategoryId equals ct.Id
+            //              select new MovieCategoryDto
+            //              {
+            //                  Id = mv.Id,
+            //                  CategoryId = mv.CategoryId,
+            //                  Director = mv.Director,
+            //                  ImageUrl = mv.ImageUrl,
+            //                  ImdbRate = mv.ImdbRate,
+            //                  Name = mv.Name,
+            //                  CategoryName = ct.Name
+            //              });
+
+
+
         }
     }
 }
