d71b4678dd96b612c60b9cf73bd1774b54ad5080
Cugurel <cugurel7@gmail.com>
2024-04-26 20:09:00 +0300

    updates

--- WebApp/Controllers/RoleController.cs
+++ WebApp/Controllers/RoleController.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Identity;
 using WebApp.Models.Identity;
 
 namespace WebApp.Controllers
 {
     public class RoleController : Controller
     {
+        UserManager<User> _userManager;
+        RoleManager<IdentityRole> _roleManager;
 
-        public IActionResult Index()
+        public RoleController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
-            return View();
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
         }
 
         [HttpGet]
@@ -18,9 +30,48 @@ namespace WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreateRole(RoleModel model)
+        public async Task<IActionResult> CreateRole(RoleModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var result = await _roleManager.CreateAsync(new IdentityRole(model.Name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
             return View();
+        }
+
+        public async Task<IActionResult> Update(string  id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            var members = new List<User>();
+            var nonMembers = new List<User>();
+
+            foreach (var user in _userManager.Users)
+            {
+                var list = await _userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+
+                list.Add(user);
+            }
+
+            var model = new RoleDetails()
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            };
+
+            return View(model);
         }
     }
 }
---
+++ WebApp/Models/Identity/RoleDetails.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebApp.Identity;
+
+namespace WebApp.Models.Identity
+{
+    public class RoleDetails
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<User> Members { get; set;}
+        public IEnumerable<User> NonMembers { get; set;}
+    }
+}
