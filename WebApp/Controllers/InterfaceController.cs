15fb1b88937f42942811f799e2616e904cf427cb
Cugurel <cugurel7@gmail.com>
2024-06-03 20:44:24 +0300

    ön yüz

---
+++ WebApp/Controllers/InterfaceController.cs
@@ -0,0 +1,19 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class InterfaceController : Controller
+    {
+        IMovieService _movieService;
+        public InterfaceController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        public IActionResult Index()
+        {
+            var movies = _movieService.GetMoviesWithCategory();
+            return View(movies);
+        }
+    }
+}
