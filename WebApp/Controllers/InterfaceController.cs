39f3e847880b431378304bd3132c15fae37aa94d
Cugurel <cugurel7@gmail.com>
2024-06-05 20:16:49 +0300

    top rated and most commented

--- WebApp/Controllers/InterfaceController.cs
+++ WebApp/Controllers/InterfaceController.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Controllers
@@ -6,6 +7,7 @@ namespace WebApp.Controllers
     public class InterfaceController : Controller
     {
         IMovieService _movieService;
+        Context c = new Context();
         public InterfaceController(IMovieService movieService)
         {
             _movieService = movieService;
@@ -13,6 +15,9 @@ namespace WebApp.Controllers
         public IActionResult Index()
         {
             var movies = _movieService.GetMoviesWithCategory();
+
+            ViewBag.TopRatedMovies = c.Movies.Where(x => x.CategoryId == 13).ToList();
+            ViewBag.MostCommentedMovies = c.Movies.Where(x => x.CategoryId == 14).ToList();
             return View(movies);
         }
     }
